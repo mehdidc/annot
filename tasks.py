f6100f2e1b6f38b7242bb62af0a28a1ab3a16b07
Mehdi Cherti <mehdicherti@gmail.com>
2016-05-16 00:37:40 +0200

    update

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import os
 
+from skimage.io import imread
 from invoke import task
 from app import db, Image
 from img_serve import get_all_imgs
@@ -16,8 +17,19 @@ def insert_urls(filename):
     db.session.commit()
 
 @task
-def insert_local():
-    for filename in get_all_imgs('.'):
+def insert_local(folder='imgs'):
+    for filename in get_all_imgs(folder):
+        if not accept(filename):
+            print("{} not accepted".format(filename))
+            continue
         url = '{LOCAL}/' + filename
         db.session.add(Image(url=url))
+        print("Adding {}...".format(filename))
     db.session.commit()
+
+def accept(filename):
+    val = imread(filename).sum()
+    if val > 0:
+        return True
+    else:
+        return False
